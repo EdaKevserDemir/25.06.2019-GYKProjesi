0687607c861291861093c92e7c6e79b0d6fe2583
EdaKevserDemir <31306626+EdaKevserDemir@users.noreply.github.com>
2019-06-25 22:02:46 +0300

    Add files via upload

---
+++ src/main/java/com/example/bilgisayar/gykproje/fragments/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.example.bilgisayar.gykproje.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.bilgisayar.gykproje.R;
+
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+
+    }
+}
